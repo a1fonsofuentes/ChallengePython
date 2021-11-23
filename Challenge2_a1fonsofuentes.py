dd7f8833da64b477e4fd0343a0ee7dde79ad6a3e
a1fonsofuentes <ajfuentest@gmail.com>
2021-11-23 12:35:13 +0100

    Actualizados

--- Challenge2_a1fonsofuentes.py
+++ Challenge2_a1fonsofuentes.py
@@ -1,4 +1,4 @@
 print("Welcome to the Miles Per Hour Conversion App.")
 miles = float(input("What is your speed in miles per hour? "))
-meters_per_hour = round(miles * 0.4474, 2)
+meters_per_hour = round(miles * 0.4474, 2) #usamos round para 
 print(f"Your speed in meters per hour is {meters_per_hour}.")
