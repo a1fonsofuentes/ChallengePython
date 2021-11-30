d8b5943e482ee8f8f386346691abb26e0c83c6f3
Alfonso Fuentes <94533010+a1fonsofuentes@users.noreply.github.com>
2021-11-30 12:49:44 +0100

    Delete Strings directory

--- Strings/Challenge2_a1fonsofuentes.py
+++
@@ -1,5 +0,0 @@
-print("Welcome to the Miles Per Hour Conversion App.")
-miles = float(input("What is your speed in miles per hour? "))
-meters_per_hour = round(miles * 0.4474, 2) #usamos round para redondear.
-print(f"Your speed in meters per hour is {meters_per_hour}.") #printf para escribir todo en una linea
-print("hola")
